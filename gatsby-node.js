90ef933ca37c8db06d948051400867bd5bb9362b
nexseedcamille <ykamimori@nexseed.net>
2020-11-25 15:22:55 +0800

    仮

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
 	// Define a template for blog post
 	const blogPost = path.resolve(`./src/templates/blog-post.js`)
-	const blogList = path.resolve(`./src/templates/blogs.js`)
+	const blogList = path.resolve(`./src/pages/blogs.js`)
 
 	// Get all markdown blog posts sorted by date
 	const result = await graphql(
