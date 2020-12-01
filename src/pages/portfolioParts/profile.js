2d16ec57413f617cc31dc0c607fada446cb31b50
nexseedcamille <ykamimori@nexseed.net>
2020-12-01 18:04:39 +0800

    ファイルインポートし直し
    文字化けていたので修正

---
+++ src/pages/portfolioParts/profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import VoiceStyles from "./css/voice.module.css"
+import CommonStyles from "./css/common.module.css"
+import Feature from './feature'
+import History from './history'
+import Faq from './faq'
+
+const Voice = () => (
+	<section className={CommonStyles.myMd}>
+		<header className={CommonStyles.headingLg}>
+			<h2>Profile</h2>
+		</header>
+		<div className={CommonStyles.myMd} class="l-container" >
+			<div class="c-profile__img u-mblg">
+				<img class="c-profile__img u-mblg" src="/static/b0464912775ee836bf92057ad4afa07f/b4294/my-profile.jpg" />
+			</div>
+			<h3 className={CommonStyles.headingSm}>かみーゆ（ビダンじゃないほう）/フロントエンドエンジニア</h3>
+			<p>広島生まれ広島育ちのIT戦士です。気が付いたらフロントエンドエンジニアになっていました。<br />さらに最近気が付いたのですが、いつの間にかフィリピン・セブ島に来て、しかもロックダウン（コミュニティ単位での隔離）に巻き込まれてます。人生っておもしろい。</p>
+		</div>
+		<div className={CommonStyles.bg_gray}>
+			<Feature />
+			<hr className={CommonStyles.hr} />
+			<History />
+			<hr className={CommonStyles.hr} />
+			<Faq />
+		</div>
+	</section>
+);
+
+export default Voice;
