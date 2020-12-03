f13734514fecd4001efa7292841318743c7d4678
nexseedcamille <ykamimori@nexseed.net>
2020-12-03 20:38:03 +0800

    コード変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,4 @@
 module.exports = {
-	plugins: [
-		{
-			resolve: "gatsby-plugin-google-analytics",
-			options: {
-				trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-				head: true,
-				respectDNT: true,
-			}
-		},
-	],
 	siteMetadata: {
 		title: `銀ねこアトリエ`,
 		author: {
@@ -58,23 +48,20 @@ module.exports = {
 	},
 	plugins: [
 		{
-			resolve: `gatsby-theme-blog`,
+			resolve: "gatsby-plugin-google-analytics",
 			options: {
-				basePath: `/posts`,
-			},
+				trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+				head: true,
+			}
 		},
-	],
-	plugins: [
 		{
 			resolve: `gatsby-plugin-fontawesome-css`
-		}
-	],
-	plugins: [
+		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				path: `${__dirname}/content/posts`,
-				name: `blog`,
+				name: `posts`,
 
 			},
 		},
@@ -124,12 +111,6 @@ module.exports = {
 		},
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				//trackingId: `ADD YOUR TRACKING ID HERE`,
-			},
-		},
 		`gatsby-plugin-feed`,
 		{
 			resolve: `gatsby-plugin-manifest`,
