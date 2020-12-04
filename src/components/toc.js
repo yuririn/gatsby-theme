957c8e8aa047c49b55e5794d27a801df263ee5dc
nexseedcamille <ykamimori@nexseed.net>
2020-12-05 06:42:19 +0800

    仮ディプロイ

--- src/components/toc.js
+++ src/components/toc.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Toc = props => {
+	const list = props.data.replace(/(ul>)/gi, 'ol>');
+
 	return (
 		<div class="p-box--gray u-mblg">
 			<h2 className="c-content__heading">この記事のざっくりとした内容</h2>
 			<div className="c-editArea">
 				<div
 					dangerouslySetInnerHTML={{
-						__html: props.data,
+						__html: list,
 					}}
 				>
 				</div>
