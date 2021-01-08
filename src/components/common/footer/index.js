b699f399005248f6b122c70a2da2f5866b414133
nexseedcamille <ykamimori@nexseed.net>
2021-01-08 09:10:49 +0800

    add: ブログ追加

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -1,7 +1,4 @@
 import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 import FooterNav from "./footernav"
 
