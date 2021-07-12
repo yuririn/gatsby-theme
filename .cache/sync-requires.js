47678ed20a89e5721ebfcc5e43682ee4b0782b5b
yuririn <yuri.soushi@gmail.com>
2021-07-12 12:36:33 +0800

    fix everything

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/kamile/Documents/sourcetree/gatsby-theme/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/kamile/Documents/sourcetree/gatsby-theme/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/kamile/Documents/sourcetree/gatsby-theme/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/kamile/Documents/sourcetree/gatsby-theme/src/pages/index.js")),
