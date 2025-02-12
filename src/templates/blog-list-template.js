80df1a98c34336fbabbbb19e942341918486e587
Camile-lenz <kamimori@lenz-ph.com>
2025-02-12 13:02:24 +0800

    Updated src/templates/blog-list-template.js.

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -1,31 +1,29 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
-
+import { graphql } from "gatsby"
+import Search from "../components/search"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import SideBar from "../components/SideBar"
 import InfiniteScrollComponent from "../components/posts/InfiniteScrollComponent";
 
 
 const BlogList = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
 
   return (
     <Layout location={location} title={siteTitle}>
         <header className={`c-page-header`} id="keyvisual">
-            <h1>セブ島ノマドブログ</h1>
-              <p>現在{data.allMarkdownRemark.totalCount} 記事あります</p>
+            <h1>セブ島エンジニアのノマドブログ</h1>
+              <p>現在  {data.allMarkdownRemark.totalCount} 記事あります</p>
         </header>
         
-      <div className="l-section">
-        <div className="l-container--blog__main">
-            
-
-            <InfiniteScrollComponent />
+        <div className="l-section l-container-archive">
+            <div>
+                <InfiniteScrollComponent/>
+            </div>
+              <div className="l-container-archive__sticky-area">
+              <Search></Search>
+            </div>
         </div>
-        <SideBar></SideBar>
-      </div>
     </Layout>
   )
 }
