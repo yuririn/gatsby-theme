1e46b526b89234cb5e8fba1385364a0793981393
Camile-lenz <kamimori@lenz-ph.com>
2025-02-12 13:06:47 +0800

    Modifed and added footer component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,24 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Header from "./common/Header";
+import Footer from "./common/footer";
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
 
-    // if (typeof window !== "undefined") {
-    //     const setTheme = newTheme => (document.body.className = newTheme)
-    //     const mode = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
-    //     setTheme(mode)
-    // }
+    if (typeof window !== "undefined") {
+        const setTheme = newTheme => (document.body.className = newTheme)
+        const mode = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+        setTheme(mode)
+    }
 
 
   return (
     <>
       <Header isRootPath={isRootPath}></Header>
       <main>{children}</main>
+          <Footer></Footer>
     </>
   )
 }
