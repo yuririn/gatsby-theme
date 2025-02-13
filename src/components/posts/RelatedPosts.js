911c4892920d94ba1f7b35df54b23821bb82b33c
Camile-lenz <kamimori@lenz-ph.com>
2025-02-14 07:30:30 +0800

    Added related posts component.

---
+++ src/components/posts/RelatedPosts.js
@@ -0,0 +1,9 @@
+import React, { useMemo } from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+const RelatedPosts = (data) => {
+    return (
+        <></>
+    )
+}
+export default RelatedPosts
