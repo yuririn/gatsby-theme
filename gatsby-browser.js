a79d45e7c9cc952bafe623c621a0655b476c924d
Camile-lenz <kamimori@lenz-ph.com>
2025-02-13 12:41:41 +0800

    機能追加
    - ダークモード切り替え
    - スムーズスクロール
    "

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,11 @@
 // Highlighting for code blocks
 import "prismjs/themes/prism-okaidia.css";
 
-// export const onRouteUpdate = ({ location, prevLocation }) => {
-//     // ページ遷移後にスクロール位置をトップにリセット
-//     window.scrollTo(0, 0);
-
-//     // スクロールアニメーションを無効化
-//     document.documentElement.style.scrollBehavior = 'auto';
-
-//     // 遷移後に再度アニメーションを有効化
-//     setTimeout(() => {
-//         document.documentElement.style.scrollBehavior = 'smooth';
-//     }, 0);
-// };
+export const onRouteUpdate = ({ location }) => {
+    if (location.hash) {
+        // 現在のURLにハッシュが含まれている場合はスクロールをやめさせる
+        return;
+    }
+    // ページ遷移後にスクロール位置をトップにリセット
+    window.scrollTo(0, 0);
+};
