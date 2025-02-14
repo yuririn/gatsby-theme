85cc403181f7d0bbfd5acfae7837e00d5868eb84
Camile-lenz <kamimori@lenz-ph.com>
2025-02-15 04:36:10 +0800

    Modified 404 page setting.

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -6,38 +6,50 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 import { siteMetadata } from "../../../gatsby-config"
 import JsonLD from "./JsonLD"
 
 const Seo = ({ data, location }) => {
     const { title, description, template } = data
-    const isRoot = location.pathname === '/' ? true :false
+    const isRoot = location.pathname === '/' ? true : false
     //タイトルとディスクリプション
     let pageTitle = isRoot ? siteMetadata.title : `${title} - ${siteMetadata.title}`
     let pageDescription = isRoot ? siteMetadata.description : description
 
-  return (
-    <>
-        <title>{pageTitle}</title>
-          <meta name="description" content={pageDescription} />
-          <meta property="og:title" content={pageTitle} />
-      <meta property="og:description" content={pageDescription} />
-          <meta property="og:type" content={isRoot ? `website`: `webpage`} />
-      <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:creator"
-        content={siteMetadata?.social?.twitter || ``}
-      />
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link href={`/${template + '-' || 'blog-' }style.css`} rel="stylesheet" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true}/>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet"/>
-      <meta name="twitter:title" content={pageTitle} />
-      <meta name="twitter:description" content={pageDescription} />
-        <JsonLD data={data} location={location}></JsonLD>
-    </>
-  )
+    //404の場合はNoindex
+    if(data.is404) {
+        return (
+            <>
+                <title>{pageTitle}</title>
+                <meta name="description" content={pageDescription} />
+                <meta name="robots" content="noindex" />
+                <link href={`/${template + '-' || 'blog-'}style.css`} rel="stylesheet" />
+            </>
+        )
+    }
+
+    return (
+        <>
+            <title>{pageTitle}</title>
+            <meta name="description" content={pageDescription} />
+            <meta property="og:title" content={pageTitle} />
+            <meta property="og:description" content={pageDescription} />
+            <meta property="og:type" content={isRoot ? `website` : `webpage`} />
+            <meta name="twitter:card" content="summary" />
+            <meta
+                name="twitter:creator"
+                content={siteMetadata?.social?.twitter || ``}
+            />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link href={`/${template + '-' || 'blog-'}style.css`} rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
+            <meta name="twitter:title" content={pageTitle} />
+            <meta name="twitter:description" content={pageDescription} />
+            <JsonLD data={data} location={location}></JsonLD>
+        </>
+    )
 }
 
 export default Seo
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,49 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql,Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/Seo/Seo"
+import SideBar from "../components/SideBar"
+import BreadCrumbList from "../components/common/BreadcrumbList"
+import Msg from "../components/posts/modueles/msg"
 
 const aboutMeta = {
     template: 'page',
-    title: '【セブ島海外ノマド】フロントエンドエンジニアかみーゆを力一杯紹介します',
-    description: `海外ノマドって何？エンジニアってどんな人でもなれるの？プログラマーって子どもいてもバツイチでも30歳過ぎていてもなれるの？生きていれば逆境なんて跳ね除けることはできます。`
+    title: 'このページは宇宙の彼方へ消えました',
+    description: `お探しのページは見つかりませんでした。`
 }
 
 const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+    const { title } = aboutMeta
+    const breadCrumbList = {
+        current: title
+    }
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+      <Layout location={location}>
+          <div className="l-main_contents is-page">
+              <header className="c-page-header en" id="keyvisual">
+                <div>
+                      <h1>404<span>{title}</span></h1>
+                  
+                </div>
+                  <BreadCrumbList list={breadCrumbList} ></BreadCrumbList>
+              </header>
+              <div className="l-section l-container--page">
+
+                <section itemProp="articleBody" className="c-post-body">
+                      <p>まるで探しているページがブラックホールに吸い込まれたかのようです。でも心配しないでください！Webサイトの残りの部分はまだ無事です。</p>
+                      <ul>
+                          <li>代わりに、ホームページに戻ってみませんか？</li>
+                          <li>探したいページを検索機能で探してみてください。PCなら右側、モバイルならスクロールダウンすれば探せますよ！</li>
+                          <li>お探しの情報が見つからない場合は、<Link to={`/contact/`}>お問い合わせ</Link>からメッセージをお送りください。気が向いたらお返事します。</li>
+                      </ul>
+                      <Msg txt="次回、エイリアンがページを持ち去る前に訪れることをわすれないでねーーーーん。"></Msg>
+                </section>
+                  <SideBar></SideBar>
+              </div>
+          </div>
+      </Layout>
   )
 }
 
@@ -34,10 +59,11 @@ export const Head = ({ location }) => {
     return <Seo
         location={location}
         data={{
-            template: 'blog',
+            template: 'page',
             title: title,
             description: description,
-            list: list
+            list: list,
+            is404: true
         }}
     />
 }
