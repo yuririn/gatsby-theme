dd7543869d642bdf81b1bc5324270670ab2779c2
Camile-lenz <kamimori@lenz-ph.com>
2025-02-14 13:31:22 +0800

    Finished building this function.

--- src/components/common/img.js
+++ src/components/common/img.js
@@ -14,7 +14,7 @@ const Img = ({ source, title, className, sizes }) => {
                 gatsbyImageData(
                   blurredOptions: { width: 100 }
                   width: 640
-                  quality: 60
+                  quality: 40
                   placeholder: BLURRED
                 )
               }
@@ -29,11 +29,23 @@ const Img = ({ source, title, className, sizes }) => {
 
     let img = allFile.edges.find(img => img.node.relativePath === resource)
 
-
     if (img) {
+        let image;
+        if (sizes && sizes.length === 3) {
+            const [width, height, quality] = sizes;
+            image = getImage({
+                ...img.node.childImageSharp.gatsbyImageData,
+                width: width, // 動的な幅
+                height: height, // 動的な高さ
+                quality: quality // 動的なクオリティ
+            });
+        } else {
+            image = getImage(img.node.childImageSharp.gatsbyImageData);
+        }
+
         return (
             <GatsbyImage
-                image={getImage(img.node.childImageSharp.gatsbyImageData)}
+                image={image}
                 alt={title}
                 key={title}
                 className={className}
@@ -43,4 +55,5 @@ const Img = ({ source, title, className, sizes }) => {
         return ""
     }
 }
+
 export default Img
