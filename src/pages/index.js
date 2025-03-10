8b63636e768b3afb5a91da9de7e0875d2e40f5c8
Camile-lenz <kamimori@lenz-ph.com>
2025-03-10 11:55:48 +0800

    記事追加。

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link, graphql } from "gatsby"
 
 import Seo from "../components/seo"
 
+import Img from "../components/common/img"
+
 import Layout from "../components/layout"
 import FirstView from "../components/top-first-view"
 import Post from "../components/posts/posts"
@@ -21,6 +23,15 @@ const BlogIndex = ({ data, location }) => {
             <FirstView></FirstView>
             <div className="l-section l-container--blog">
                 <div className="l-container--blog__main">
+                  <header className="c-heading--lg"><h2>Greeting</h2><p>セブから学んだこと</p></header>
+                  <div className="c-greeting">
+                  <Img source="common/camille-pic.jpg"/>
+                    <p>こんにちは、海外ノマドエンジニアかみーゆです。セブで一念発起し、資金0で会社を設立しました。</p>
+                    <p>私はフィリピンに来て以来、人付き合いも物欲も減りました。</p>
+                    <p>この国では日本に比べ、手を伸ばせばすぐ欲しいものは手に入らないです。</p>
+                    <p>2020年のパンデミック世界最長ロックダウンを経て、多少の不便じゃ困らないし、死なないことも学びました。<br />結果、節約できるようになりました。わずかだけど親にもサポートもしています。</p>
+                    <p>これが1番フィリピンに感謝していることです。この「銀ねこアトリエ」ではそんなライフスタイルも含めて発信しています。</p>
+                  </div>
                     <header className="c-heading--lg"><h2>Pick up</h2><p>おすすめ記事</p></header>
                     <PickUpPosts></PickUpPosts>
                     <Ad location={location} adClass={`u-my--lg`}></Ad>
